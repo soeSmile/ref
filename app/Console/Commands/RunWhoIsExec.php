f04b63cf4da03cb3a6c0f7c29bbf8dfc1b2fc1d3
Smirnov <soe190715@gmail.com>
2022-02-18 12:55:43 +0700

    laravel/framework (v9.1.0)

---
+++ app/Console/Commands/RunWhoIsExec.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+use function exec;
+use function trim;
+
+/**
+ * Class RunWhoIsExec
+ */
+class RunWhoIsExec extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'whois:exec';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Run whois exec';
+
+    /**
+     * @return void
+     */
+    public function handle(): void
+    {
+        $domains = ['myref.top', 'trelo.com', 'atfn.ru'];
+
+        foreach ($domains as $domain) {
+            $output = $data = [];
+            exec('whois ' . $domain, $output, $result);
+
+            if (!$result) {
+                $data = $this->parseResult($output);
+            }
+        }
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    private function parseResult(array $data): array
+    {
+        $result = [];
+
+        foreach ($data as $item) {
+            preg_match('/(.+?):(.+)/', $item, $array);
+
+            $key = trim($array !== [] ? $array[1] : $item, ' \n\r\t\v\x00<>"');
+            $value = trim($array !== [] ? $array[2] : $item, ' \n\r\t\v\x00<>"');
+
+            $result[$key] = $value;
+        }
+
+        return $result;
+    }
+}
