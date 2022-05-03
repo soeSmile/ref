f28d32a2dc98ad65e55e00ce844b457af7261eb6
Smirnov <soe190715@gmail.com>
2022-05-03 15:59:43 +0700

    Laravel Framework 9.10.1

---
+++ app/Http/Controllers/Api/UserApiController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+/**
+ * @package Api
+ */
+class UserApiController
+{
+    /**
+     * @return void
+     */
+    public function index(): void
+    {
+    }
+}
